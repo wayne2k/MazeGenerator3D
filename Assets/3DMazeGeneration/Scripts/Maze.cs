9c7126ac9c1c7578d866f237d014d50de045fe0f
wayne2k <wayne2k@gmail.com>
2015-02-25 23:48:30 +0200

    Random Cell Generation
    Generate random amount of maze cells at  random direction.

--- Assets/3DMazeGeneration/Scripts/Maze.cs
+++ Assets/3DMazeGeneration/Scripts/Maze.cs
@@ -8,18 +8,32 @@ public class Maze : MonoBehaviour
 	public float generationStepDelay = 0.01f;
 
 	private MazeCell[,] cells;
-	
+
+	public MazeCell GetCell (IntVector2 coordinates) {
+		return cells[coordinates.x, coordinates.z];
+	}
+
 	public IEnumerator Generate () {
 		WaitForSeconds delay = new WaitForSeconds(generationStepDelay);
 		cells = new MazeCell[size.x, size.z];
-		for (int x = 0; x < size.x; x++) {
-			for (int z = 0; z < size.z; z++) {
-				yield return delay;
-				CreateCell(new IntVector2(x, z));
-			}
+		IntVector2 coordinates = RandomCoordinates;
+		while (ContainsCoordinates(coordinates) && GetCell(coordinates) == null) {
+			yield return delay;
+			CreateCell(coordinates);
+			coordinates += MazeDirections.RandomValue.ToIntVector2();
+		}
+	}
+
+	public IntVector2 RandomCoordinates {
+		get {
+			return new IntVector2(Random.Range(0, size.x), Random.Range(0, size.z));
 		}
 	}
 	
+	public bool ContainsCoordinates (IntVector2 coordinate) {
+		return coordinate.x >= 0 && coordinate.x < size.x && coordinate.z >= 0 && coordinate.z < size.z;
+	}
+
 	private void CreateCell (IntVector2 coordinates) {
 		MazeCell newCell = Instantiate(cellPrefab) as MazeCell;
 		cells[coordinates.x, coordinates.z] = newCell;
