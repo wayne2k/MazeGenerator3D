e2a999a748c05484e9ed932a489f35573671b102
wayne2k <wayne2k@gmail.com>
2015-02-25 23:28:21 +0200

    Base Unity Project Added
    Base prefabs, scripts created.

---
+++ Assets/3DMazeGeneration/Scripts/Maze.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Maze : MonoBehaviour 
+{
+	public IntVector2 size;
+	public MazeCell cellPrefab;
+	public float generationStepDelay = 0.01f;
+
+	private MazeCell[,] cells;
+	
+	public IEnumerator Generate () {
+		WaitForSeconds delay = new WaitForSeconds(generationStepDelay);
+		cells = new MazeCell[size.x, size.z];
+		for (int x = 0; x < size.x; x++) {
+			for (int z = 0; z < size.z; z++) {
+				yield return delay;
+				CreateCell(new IntVector2(x, z));
+			}
+		}
+	}
+	
+	private void CreateCell (IntVector2 coordinates) {
+		MazeCell newCell = Instantiate(cellPrefab) as MazeCell;
+		cells[coordinates.x, coordinates.z] = newCell;
+		newCell.coordinates = coordinates;
+		newCell.name = "Maze Cell " + coordinates.x + ", " + coordinates.z;
+		newCell.transform.parent = transform;
+		newCell.transform.localPosition =
+			new Vector3(coordinates.x - size.x * 0.5f + 0.5f, 0f, coordinates.z - size.z * 0.5f + 0.5f);
+	}
+}
---
+++ Assets/3DMazeGeneration/Scripts/MazeCell.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MazeCell : MonoBehaviour 
+{
+	public IntVector2 coordinates;
+}
